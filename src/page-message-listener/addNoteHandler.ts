150737376bfa8a4a974dbcc8b52bf7f761afedef
Alexey Yunoshev <alexey.yunoshev@carrier.com>
2022-06-30 21:26:40 +0400

    update

--- src/page-message-listener/addNoteHandler.ts
+++ src/page-message-listener/addNoteHandler.ts
@@ -4,6 +4,7 @@ import { AddNoteResponseMessage } from "../messages";
 export function addNoteHandler(message: AddNoteResponseMessage) {
   if (message.payload.error !== null) {
     logger.error(message.payload.error);
+    alert(`Error adding note. Is Anki running?`)
   } else {
     logger.info("Note added");
   }

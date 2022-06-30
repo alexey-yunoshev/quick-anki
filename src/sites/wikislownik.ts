7c9ba412b8d63a4a8e6f937423db37c582b409a3
Alexey Yunoshev <alexey.yunoshev@carrier.com>
2022-06-30 22:32:07 +0400

    update

--- src/sites/wikislownik.ts
+++ src/sites/wikislownik.ts
@@ -2,6 +2,106 @@ import { makeAddToAnkiButton } from "../components/addToAnkiButton";
 import { registerMessageListener } from "../page-message-listener";
 import { waitForElements } from "../utils/waitForSelector";
 
+import { getLemmaLevel } from "../data/getLemmaLevel";
+import { polishLemmasLevelMap } from "../data/languages/polish";
+import { logger } from "../logger";
+
+export interface FormatSameRootLemmasOptions {
+  showLemmaLevel?: boolean;
+  hideRedLinks?: boolean;
+}
+
+function isLemmaLink(element: Element): element is HTMLAnchorElement {
+  return (
+    (element as HTMLAnchorElement).href !== undefined && element.tagName === "A"
+  );
+}
+
+export function formatSameRootLemmas() {
+  const titleElement = document.querySelector("dt.fldt-pokrewne");
+  logger.info({
+    fn: 'formatSameRootLemmas',
+    message: 'titleElement',
+    titleElement,
+  });
+  if (titleElement === null) {
+    return;
+  }
+
+  const container = titleElement.parentElement;
+  logger.info({
+    fn: 'formatSameRootLemmas',
+    message: 'container',
+    container,
+  });
+  if (container === null) {
+    return;
+  }
+  const sections = document.querySelectorAll("dd.fldt-pokrewne");
+  logger.info({
+    fn: 'formatSameRootLemmas',
+    message: 'sections',
+    sections,
+  });
+  if (sections.length === 0) {
+    return;
+  }
+
+  const lemmasByLevel = new Map<string, Array<{ lemma: string; url: string }>>();
+
+  for (const section of sections) {
+    for (const child of Array.from(section.children)) {
+      if (isLemmaLink(child) && !child.classList.contains("new")) {
+        const lemma = child.innerText;
+        const lemmaLevel = getLemmaLevel(polishLemmasLevelMap, lemma);
+
+        if (lemmaLevel !== undefined) {
+          if (lemmasByLevel.has(lemmaLevel)) {
+            lemmasByLevel.get(lemmaLevel)?.push({
+              url: child.href,
+              lemma,
+            });
+          } else {
+            lemmasByLevel.set(lemmaLevel, [{
+              url: child.href,
+              lemma,
+            }]);
+          }
+        }
+      }
+    }
+  }
+
+  logger.info({
+    fn: 'formatSameRootLemmas',
+    message: 'lemmasByLevel',
+    lemmasByLevel,
+  });
+
+  if (lemmasByLevel.size > 0) {
+    const buttons: Array<HTMLButtonElement> = [];
+
+    for (const [level, urls] of Array.from(lemmasByLevel.entries()).sort(([level1], [level2]) => level1.localeCompare(level2))) {
+      const openUrlsButton: HTMLButtonElement = document.createElement("button");
+      openUrlsButton.innerText = `Open all ${level} level lemmas`;
+
+      openUrlsButton.addEventListener("click", () => {
+        for (const { url } of urls) {
+          window.open(url, "_blank");
+        }
+      });
+
+      buttons.push(openUrlsButton);
+    }
+
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.replaceChildren(...buttons);
+    buttonsContainer.classList.add("root_links_container", "ml-25");
+    titleElement.insertAdjacentElement("afterend", buttonsContainer);
+  }
+}
+
+
 export async function updateWikislownikArticle(
 ): Promise<void> {
   const translationElements = await waitForElements<HTMLLIElement>("div.mw-parser-output > ul > li.fldt-tlumaczenia");
@@ -26,6 +126,8 @@ export async function updateWikislownikArticle(
     });
     translationElement.insertAdjacentElement("beforeend", ankiButton);
   }
+
+  formatSameRootLemmas();
 
   registerMessageListener({
     deckPickerLocation: {
